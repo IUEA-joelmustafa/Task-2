ebf6a5e292e7f1069ad2528b5c9118184ad217b8
hermonj-prog <hermonjp@gmail.com>
2026-03-16 18:37:50 +0300

    Initial commit
    Added a borderLayout frame with buttons and textarea

---
+++ BorderFrameLayout/src/frameWin.java
@@ -0,0 +1,58 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class frameWin {
+    JFrame frame;
+
+    public frameWin(){
+        createFrame();
+    }
+
+    void createFrame() {
+        //  frame creation
+        frame = new JFrame("Border Layout");
+        frame.setSize(600,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        //  North Lable
+
+        JLabel titleLable = new JLabel("Application Title Demo",JLabel.CENTER);
+        frame.add(titleLable,BorderLayout.NORTH);
+
+
+        //  west panels and buttons
+
+        JPanel westPanel = new JPanel();
+        westPanel.setLayout(new GridLayout(3,1));
+
+        westPanel.add(createButtonOne(), BorderLayout.WEST);
+        westPanel.add(createButtonTwo(), BorderLayout.WEST);
+        westPanel.add(createButtonThree(), BorderLayout.WEST);
+        frame.add(westPanel, BorderLayout.WEST);
+
+        //  center textarea
+
+        JTextArea textArea = new JTextArea();
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        frame.add(scrollPane,BorderLayout.CENTER);
+
+        frame.setVisible(true);
+
+    }
+
+    //   buttons
+
+    JButton createButtonOne(){
+        JButton buttonOne = new JButton("Option 1");
+        return buttonOne;
+    }
+    JButton createButtonTwo(){
+        JButton buttonTwo = new JButton("Option 2");
+        return buttonTwo;
+    }
+    JButton createButtonThree(){
+        JButton buttonThree = new JButton("Option 3");
+        return buttonThree;
+    }
+}
